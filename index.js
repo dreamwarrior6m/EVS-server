6eda27e2519e0742711a2d6eeb9a482f80cea165
Anamul9901 <anamulhaque9901@gmail.com>
2024-01-22 18:31:20 +0600

    more setup

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://electronic-voting-system-one.vercel.app',
+  ]
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7aawhhw.mongodb.net/?retryWrites=true&w=majority`;
