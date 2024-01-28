0c88e4081829aa636692509dda5deab0b802eb0a
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-01-28 22:17:41 +0600

    added pagination backend create by server site

--- index.js
+++ index.js
@@ -111,6 +111,39 @@ async function run() {
       const cursor = await createVoteCollection.find().toArray();
       res.send(cursor);
     });
+    // pagination
+    app.get('/paginatedUsers', async (req, res) => {
+      try {
+        const allUser = await userCollection.find({}).toArray();
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+    
+        const startIndex = (page - 1) * limit;
+        const lastIndex = page * limit;
+    
+        const results = {};
+        results.totalUser = allUser.length;
+        results.pageCount = Math.ceil(allUser.length / limit);
+    
+        if (lastIndex < allUser.length) {
+          results.next = {
+            page: page + 1,
+          };
+        }
+    
+        if (startIndex > 0) {
+          results.prev = {
+            page: page - 1,
+          };
+        }
+    
+        results.result = allUser.slice(startIndex, lastIndex);
+        res.json(results);
+      } catch (error) {
+        console.error('Error fetching paginated users:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
+    });
 
 
 
