50fc225615581b85dbbb488ce60df08c222233cc
Anamul9901 <anamulhaque9901@gmail.com>
2024-02-03 20:24:03 +0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rsjkylo.mongodb.net/?retryWrites=true&w=majority`;
@@ -28,6 +28,9 @@ async function run() {
     const userCollection = client.db("dvsDB").collection("users");
     const candidateCollection = client.db("dvsDB").collection("candidate");
     const createVoteCollection = client.db("dvsDB").collection("create-vote");
+    const participateVoteCollection = client
+      .db("dvsDB")
+      .collection("participate");
 
     app.post("/users", async (req, res) => {
       const newUser = req.body;
@@ -35,72 +38,131 @@ async function run() {
       const result = await userCollection.insertOne(newUser);
       res.send(result);
     });
-    
 
     app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
     // user verify for admin
-    app.patch('/users/verify/:id',async(req,res)=>{
-      const id = req.params.id
-      const query = {_id : new ObjectId(id)}
-      console.log(query)
+    app.patch("/users/verify/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
       const doc = {
-        $set:{
-          verify : 'true'
-        }
-      }
-      const result = await userCollection.updateOne(query,doc)
-      console.log(result)
-      res.send({message:true})
-    })
+        $set: {
+          verify: "true",
+        },
+      };
+      const result = await userCollection.updateOne(query, doc);
+      console.log(result);
+      res.send({ message: true });
+    });
 
     app.get("/users/:id", async (req, res) => {
-      const id = req.params.id
-      console.log(id)
-      const result = await userCollection.findOne({_id: new ObjectId(id)})
+      const id = req.params.id;
+      console.log(id);
+      const result = await userCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     });
 
     //candidate releted api
     app.post("/candidate", async (req, res) => {
       const isExcits = await candidateCollection.findOne({
-        $or:
-        [
-        {candidateEmail: req.body.candidateEmail},
-        {candidateID: req.body.candidateID}
-        ]
-      })
-      if(isExcits){
-        return res.status(400).send({message:"This Candidate is wrong"})
-      }
-      else{
+        $or: [
+          { candidateEmail: req.body.candidateEmail },
+          { candidateID: req.body.candidateID },
+        ],
+      });
+      if (isExcits) {
+        return res.status(400).send({ message: "This Candidate is wrong" });
+      } else {
         const newCandidate = req.body;
         const result = await candidateCollection.insertOne(newCandidate);
         res.send(result);
       }
     });
-    // user deleted
-    app.delete('/users/:id',async(req,res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
+
     // candidate deleted
-    app.delete('/candidate/:id',async(req,res)=>{
+    app.delete("/candidate/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await candidateCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await candidateCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.get("/candidate", async (req, res) => {
       const cursor = await candidateCollection.find().toArray();
       res.send(cursor);
     });
 
+    app.get("/candidate/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await candidateCollection.findOne({
+        _id: new ObjectId(id),
+      });
+      // console.log(id);
+      res.send(result);
+    });
+
+    app.patch("/candidate/:id", async (req, res) => {
+      const data = req.params;
+      const data2 = req.body;
+      // console.log(data2)
+      const filter = { _id: new ObjectId(data.id) };
+      // console.log(filter);
+      const updateDoc = {
+        $set: {
+          voteCount: data2.updateVoteCount2,
+        },
+      };
+      const result = await candidateCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
+    // user deleted
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //participate vote releted api
+    app.get("/participate", async (req, res) => {
+      const result = await participateVoteCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.post("/users", async (req, res) => {
+    //   const newUser = req.body;
+    //   console.log("new user:", newUser);
+    //   const result = await userCollection.insertOne(newUser);
+    //   res.send(result);
+    // });
+
+    app.post("/participate", async (req, res) => {
+      const newParticipate = req.body;
+      console.log("newParticipate", newParticipate);
+      const result = await participateVoteCollection.insertOne(newParticipate);
+      res.send(result);
+    });
+
     // create-vor realeted api
+    app.patch("/create-vote/:name", async (req, res) => {
+      const data = req.params;
+      const data2 = req.body;
+      console.log(data2);
+      const filter = { name: data.name };
+      console.log(filter);
+      const updateDoc = {
+        $set: {
+          voterEmail: data2.updateVoterEmail,
+        },
+      };
+      const result = await createVoteCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
     app.post("/create-vote", async (req, res) => {
       const newCreateVote = req.body;
       const result = await createVoteCollection.insertOne(newCreateVote);
@@ -111,41 +173,48 @@ async function run() {
       const cursor = await createVoteCollection.find().toArray();
       res.send(cursor);
     });
+
+    app.get("/create-vote/:name", async (req, res) => {
+      const name = req.params.name;
+      const result = await createVoteCollection.findOne({
+        name: name,
+      });
+      res.send(result);
+    });
+
     // pagination
-    app.get('/paginatedUsers', async (req, res) => {
+    app.get("/paginatedUsers", async (req, res) => {
       try {
         const allUser = await userCollection.find({}).toArray();
         const page = parseInt(req.query.page);
         const limit = parseInt(req.query.limit);
-    
+
         const startIndex = (page - 1) * limit;
         const lastIndex = page * limit;
-    
+
         const results = {};
         results.totalUser = allUser.length;
         results.pageCount = Math.ceil(allUser.length / limit);
-    
+
         if (lastIndex < allUser.length) {
           results.next = {
             page: page + 1,
           };
         }
-    
+
         if (startIndex > 0) {
           results.prev = {
             page: page - 1,
           };
         }
-    
+
         results.result = allUser.slice(startIndex, lastIndex);
         res.json(results);
       } catch (error) {
-        console.error('Error fetching paginated users:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        console.error("Error fetching paginated users:", error);
+        res.status(500).json({ error: "Internal Server Error" });
       }
     });
-
-
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
